7fbcf668cad39033edc9810f09bb31f7caf89fc1
ありす <142141355+dev-aixel@users.noreply.github.com>
2023-08-23 15:13:47 +0900

    CSS改良＆IPを収集するように

--- input.php
+++ input.php
@@ -1,28 +1,32 @@
 <?php
-	$hour = date('H');
-	$date = date('d');
-	$hour += 8;
-	if ($hour > 23) {
-		$date += 1;
-		$hour -= 24;
-	}
-	if (!strlen($_POST['name'])) {
-        $_POST['name'] = "名無し";
-    }	
-	setcookie('username', $_POST['name'], time() + 2592000);
-	$todaydate = date('n') . "/" . $date . "/" . $hour . ":" . date('i') . ":" . date('s');
-    function inputter($putting){
-		$filename = fopen('./date/publiclog.txt', "a");
-		fputs($filename, $putting);
-		fclose($filename);
-    }
-	if (!strlen($_POST['maintext'])) {
-		header('Location: index.php');
-	}else {
-		$inter = "<p>".htmlspecialchars($_POST['name'], ENT_QUOTES|ENT_HTML5, "UTF-8") . " : " . $_POST['maintext'] ."　　　". $todaydate. "</p>" ."\n";
-	inputter($inter);
-    	header('Location: index.php');
-	}
-    
+$hour = date('H');
+$date = date('d');
+$hour += 8;
+if ($hour > 23) {
+    $date += 1;
+    $hour -= 24;
+}
+
+if (!strlen($_POST['name'])) {
+    $_POST['name'] = "名無し";
+}	
+
+setcookie('username', $_POST['name'], time() + 2592000);
+
+$todaydate = date('n') . "/" . $date . "/" . $hour . ":" . date('i') . ":" . date('s');
+
+function inputter($putting) {
+    $filename = fopen('./date/publiclog.txt', "a");
+    fputs($filename, $putting);
+    fclose($filename);
+}
+
+if (!strlen($_POST['maintext'])) {
+    header('Location: index.php');
+} else {
+    $user_ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+    $inter = "<p>".htmlspecialchars($_POST['name'], ENT_QUOTES|ENT_HTML5, "UTF-8") . " : " . $_POST['maintext'] . "　　　". $todaydate. " IP: " . $user_ip . "</p>" ."\n";
+    inputter($inter);
+    header('Location: index.php');
+}
 ?>
-<!--このチャットシステムは籘原梓 @Azusa468_Linuxが作成しました。-->
