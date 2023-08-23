cd34a2b77b00b79ceefa5e4b0392c166548c3a94
ありす <142141355+dev-aixel@users.noreply.github.com>
2023-08-23 20:07:21 +0900

    SQLITE3に対応

--- input.php
+++ input.php
@@ -1,4 +1,13 @@
 <?php
+// SQLite3データベースのファイルパス
+$databaseFile = './date/chat.db';
+
+// データベースの作成またはオープン
+$db = new SQLite3($databaseFile);
+
+// テーブルが存在しない場合は作成
+$db->exec('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, name TEXT, maintext TEXT, timestamp DATETIME)');
+
 $hour = date('H');
 $date = date('d');
 $hour += 8;
@@ -15,19 +24,24 @@ setcookie('username', $_POST['name'], time() + 2592000);
 
 $todaydate = date('n') . "/" . $date . "/" . $hour . ":" . date('i') . ":" . date('s');
 
-function inputter($putting) {
-    $filename = fopen('./date/publiclog.txt', "a");
-    fputs($filename, $putting);
-    fclose($filename);
+function inputter($db, $name, $maintext, $timestamp) {
+    $stmt = $db->prepare('INSERT INTO posts (name, maintext, timestamp) VALUES (:name, :maintext, :timestamp)');
+    $stmt->bindValue(':name', $name, SQLITE3_TEXT);
+    $stmt->bindValue(':maintext', $maintext, SQLITE3_TEXT);
+    $stmt->bindValue(':timestamp', $timestamp, SQLITE3_TEXT);
+    $stmt->execute();
 }
 
 if (!strlen($_POST['maintext'])) {
     header('Location: index.php');
 } else {
-    $user_ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
-    $inter = "<p>".htmlspecialchars($_POST['name'], ENT_QUOTES|ENT_HTML5, "UTF-8") . " : " . htmlspecialchars($_POST['maintext'], ENT_QUOTES|ENT_HTML5, "UTF-8") . "　　　". $todaydate. " IP: " . $user_ip . "</p>" ."\n";
-    inputter($inter);
+    $name = htmlspecialchars($_POST['name'], ENT_QUOTES|ENT_HTML5, "UTF-8");
+    $maintext = htmlspecialchars($_POST['maintext'], ENT_QUOTES|ENT_HTML5, "UTF-8");
+    
+    inputter($db, $name, $maintext, $todaydate);
     header('Location: index.php');
 }
+
+$db->close();
 ?>
 
